bf738b7030dcf6d272fadfda01140594d7797003
smokingplaya <smokingplaya@yandex.ru>
2025-01-08 01:45:34 +0300

    0.1.4

--- src/connection.rs
+++ src/connection.rs
@@ -2,9 +2,10 @@
 
 use std::env;
 use anyhow::Result;
-use diesel::{r2d2::{self, ConnectionManager}, PgConnection};
+use diesel::{r2d2::{self, ConnectionManager, PooledConnection}, PgConnection};
 
 pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
+pub type DbPooled = PooledConnection<ConnectionManager<PgConnection>>;
 
 pub fn establish_connection() -> Result<DbPool> {
   let database_url = env::var("DATABASE_URL")?;
