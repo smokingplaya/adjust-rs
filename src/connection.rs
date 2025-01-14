2665a853b3f39b44c23fd81d748a4f1be0b03101
smokingplaya <smokingplaya@yandex.ru>
2025-01-14 23:38:19 +0300

    0.1.8

--- src/connection.rs
+++ src/connection.rs
@@ -1,11 +1,11 @@
 use std::env;
 use anyhow::Result;
-use diesel::{r2d2::{self, ConnectionManager, Pool, PooledConnection}, PgConnection};
+use diesel::{r2d2::{ConnectionManager, Pool, PooledConnection}, PgConnection};
 use r2d2_redis::RedisConnectionManager;
 
 // PostgreSQL connection
 type Postgres = ConnectionManager<PgConnection>;
-pub type DbPool = r2d2::Pool<Postgres>;
+pub type DbPool = diesel::r2d2::Pool<Postgres>;
 pub type DbPooled = PooledConnection<Postgres>;
 
 pub fn establish_connection() -> Result<DbPool> {
@@ -16,14 +16,12 @@ pub fn establish_connection() -> Result<DbPool> {
 }
 
 // Redis connection
-type Redis = RedisConnectionManager;
-pub type RedisPool = Pool<Redis>;
-pub type RedisPooled = PooledConnection<Redis>;
+pub type RedisPool = r2d2::Pool<RedisConnectionManager>;
+pub type RedisPooled = r2d2::PooledConnection<RedisConnectionManager>;
 
 pub fn establish_redis_connection() -> Result<RedisPool> {
   let redis_url = env::var("REDIS_URL")?;
   let manager = RedisConnectionManager::new(redis_url)?;
-  let pool = Pool::builder().build(manager)?;
 
-  Ok(pool)
+  Ok(Pool::builder().build(manager)?)
 }
