3aafe9304ee5e446be1bb482bba1e88b492a85ba
smokingplaya <smokingplaya@yandex.ru>
2025-01-15 16:39:58 +0300

    0.1.11: Error update

--- src/response.rs
+++ src/response.rs
@@ -1,31 +1,53 @@
 use axum::{http::StatusCode, response::IntoResponse, Json};
 use serde::Serialize;
+use std::error::Error;
+use std::fmt::{self, Display, Formatter};
 
 #[derive(Serialize)]
-pub struct Message {
-  pub message: String
+pub struct HttpMessage {
+  pub message: String,
 }
 
-pub struct Error(pub anyhow::Error);
+#[derive(Debug)]
+pub struct HttpError(pub String, pub Option<StatusCode>);
 
-pub type Result<T> = core::result::Result<T, Error>;
+pub type HttpResult<T> = core::result::Result<T, HttpError>;
 
-impl IntoResponse for Error {
-  fn into_response(self) -> axum::response::Response {
-    (
-      StatusCode::INTERNAL_SERVER_ERROR,
-      Json(Message {
-        message: self.0.to_string()
-      })
-    ).into_response()
+impl HttpError {
+  pub fn new(msg: &str) -> HttpError {
+    HttpError(msg.to_string(), Some(StatusCode::INTERNAL_SERVER_ERROR))
+  }
+
+  pub fn new_with_code(
+    msg: &str,
+    code: StatusCode
+  ) -> HttpError {
+    HttpError(msg.to_string(), Some(code))
   }
 }
 
-impl<E> From<E> for Error
-where
-    E: Into<anyhow::Error>,
-{
-    fn from(err: E) -> Self {
-        Self(err.into())
-    }
+impl IntoResponse for HttpError {
+  fn into_response(self) -> axum::response::Response {
+    (
+      self.1.unwrap_or(StatusCode::INTERNAL_SERVER_ERROR),
+      Json(HttpMessage {
+        message: self.0.to_string(),
+      }),
+    )
+        .into_response()
+  }
+}
+
+impl Display for HttpError {
+  fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+    write!(f, "{}", self.0)
+  }
+}
+
+impl Error for HttpError {}
+
+impl From<anyhow::Error> for HttpError {
+  fn from(error: anyhow::Error) -> Self {
+    HttpError(error.to_string(), Some(StatusCode::INTERNAL_SERVER_ERROR))
+  }
 }
