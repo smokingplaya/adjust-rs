a6847283ea4b0a34342025dfd0bcccd559edb5e0
smokingplaya <smokingplaya@yandex.ru>
2025-01-23 03:10:09 +0300

    0.1.14

--- src/response.rs
+++ src/response.rs
@@ -1,6 +1,7 @@
 use axum::{http::StatusCode, response::IntoResponse, Json};
 use serde::Serialize;
 
+#[derive(Debug)]
 pub struct HttpError(pub anyhow::Error, pub Option<StatusCode>);
 
 pub type HttpResult<T> = core::result::Result<T, HttpError>;
