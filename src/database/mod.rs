0c878d0cda0230138b388b59292e4776de980a82
smokingplaya <smokingplaya@yandex.ru>
2025-02-22 03:37:57 +0300

    0.1.19

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,9 +1,7 @@
+use r2d2::PooledConnection;
+
 pub mod postgres;
 pub mod redis;
 
-// lol
 pub type Pool<T> = diesel::r2d2::Pool<T>;
-
-// todo
-#[allow(unused)]
-pub(crate) type Database<T> = T;
+pub type Database<T> = PooledConnection<T>;
